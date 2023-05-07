580f256e818ea94d9485be738bc8c08e3bbb8409
krating20 <56456483+krating20@users.noreply.github.com>
2023-05-06 20:03:43 -0700

    10 year dataset with 2 features trained

--- data/airtemp/append_final.py
+++ data/airtemp/append_final.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # Read the original CSV file
-df = pd.read_csv('2015.csv')
+df = pd.read_csv('2010.csv')
 
 # Read the second CSV file with the "air" column
-df_air = pd.read_csv('air.2015.csv')
+df_air = pd.read_csv('air.2010.csv')
 
 # Add the "air" column to the original dataframe
 df.insert(loc=4, column='air', value=df_air['air'])
 
 # Save the updated dataframe to a new CSV file
-df.to_csv('updated_air_2015.csv', index=False)
+df.to_csv('updated_air_2010.csv', index=False)
